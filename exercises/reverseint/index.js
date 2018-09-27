8c96d7fa44b668fcf3fb769f7c44d0c2c93837ec
sikun <arnazadiputra@gmail.com>
2018-09-27 10:29:00 +0700

    maxChar and reverseInt done

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,23 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let reversed= '' 
+  let nToString= n.toString()
+  for(char of nToString){
+    reversed= char + reversed 
+  }
+  return parseInt(reversed) * Math.sign(n)
+  //1
+  // if(n<0){
+  //   return parseInt(reversed) *-1
+  // }
+  // return parseInt(reversed)
+  //2
+  // if(Math.sign(n)==-1){
+  //   reversed = '-'+reversed
+  // }
+  // return parseInt(reversed)
+  }
 
 module.exports = reverseInt;
