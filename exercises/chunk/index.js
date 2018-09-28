b1b4e627a2257cc8f63029673d3eeac608ef165c
sikun <arnazadiputra@gmail.com>
2018-09-28 10:38:43 +0700

    chunk done

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,17 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const arrays=[]
+  for(el of array){
+    let lastEl= arrays[arrays.length-1]
+    if(!lastEl|| lastEl.length===size){
+      arrays.push([el])
+    }else{
+      lastEl.push(el)
+    }
+  }
+  return arrays
+}
 
 module.exports = chunk;
