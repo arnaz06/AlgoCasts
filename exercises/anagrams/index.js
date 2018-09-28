e3c6fd036179fa335a15e24bff55ff85b03e0a4d
sikun <arnazadiputra@gmail.com>
2018-09-28 15:21:48 +0700

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,32 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+  let mapStringA = mapString(stringA)
+  let mapStringB = mapString(stringB)
+  
+  if(Object.keys(mapStringA).length!==Object.keys(mapStringB).length){
+    return false
+  }
+  for (let char in mapStringA) {
+    if (mapStringA[char] !== mapStringB[char]) {
+      return false
+    }
+  }
+  return true
+}
+
+function mapString(string){
+  let chars={}
+  for (let char of string.replace(/[^\w]/g, '').toLowerCase()) {
+    if(!chars[char]){
+      chars[char]=1
+    }else{
+      chars[char]++
+    }
+  }
+  return chars
+}
 
 module.exports = anagrams;
