714e73b15a2b346438315ecc3efb7d01a6f343d3
sikun <arnazadiputra@gmail.com>
2018-10-22 19:37:47 +0700

    fix anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -37,3 +37,46 @@ function mapString(string){
 }
 
 module.exports = anagrams;
+
+
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+// function solution(S) {
+//   // write your code in JavaScript (Node.js 8.9.4)
+//   let phoneArr = S.replace(/[^\w]/g, '').split('')
+//   let phoneTemp = []
+//   let temp = []
+//   for (let i = 0; i < phoneArr.length; i++) {
+//     phoneTemp.push(phoneArr[i])
+//     temp.push(phoneArr[i])
+//     if (phoneTemp.length % 3 == 0) {
+//       temp.push("-")
+//     }
+
+//   }
+//   if (temp[temp.length - 2] == "-") {
+//     let a = temp[temp.length - 3]
+//     temp[temp.length - 3] = temp[temp.length - 2]
+//     temp[temp.length - 2] = a
+
+//   }
+//   if (temp[temp.length - 1] == "-") {
+//     let index = temp.indexOf(temp.length - 1)
+//     temp.splice(index, 1)
+//   }
+//   return temp.join("")
+
+// }
+
+// function swapModuloTwo(phone, temp) {
+//   for (let i = 0; i < phone.length; i++) {
+//     if (phone.length % 3 == 2) {
+//       let index = temp.indexOf(temp - 2)
+//       temp.splice(index, 1)
+//       temp.splice(index, 0, '-')
+//     }
+//   }
+// }
+
+// console.log(solution("0 - 22 1985--324"))
